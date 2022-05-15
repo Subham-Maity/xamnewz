9622ddb8e966d57433f793a349a4d0894425642e
Subham Maity <97989643+Subham-Maity@users.noreply.github.com>
2022-05-15 16:24:41 +0530

    Fetching API Key from News API
    Adding Api From News Api

--- src/components/News.js
+++ src/components/News.js
@@ -4,9 +4,20 @@ import NewsItem from "./NewsItem";
 export class News extends Component {
   render() {
     return (
-      <div>
-        This is a news component
-        <NewsItem />
+      <div className="container my-3">
+          <h2>XamNewz - Top Headlines</h2>
+        <div className="row">
+          <div className="col-md-4">
+            <NewsItem title="myTitle" description="mydesc"/>
+          </div>
+          <div className="col-md-4">
+            <NewsItem title="myTitle" description="mydesc"/>
+          </div>
+          <div className="col-md-4">
+            <NewsItem title="myTitle" description="mydesc"/>
+            </div>
+          </div>
+
       </div>
 
     )
