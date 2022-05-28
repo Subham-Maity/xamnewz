ee462711f19f7e19d946fedd6484c11aedd27c06
Subham Maity <97989643+Subham-Maity@users.noreply.github.com>
2022-05-29 02:43:24 +0530

    Adding Previous & Next Buttons to populate NewsItems

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,7 @@ export class NavBar extends Component {
 
     return (
         <div>
-          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
             <div className="container-fluid">
               <a className="navbar-brand" href="/">XamNewz</a>
               <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
--- src/components/News.js
+++ src/components/News.js
@@ -1,45 +1,74 @@
 import React, { Component } from 'react'
-import NewsItem from "./NewsItem";
+
+import NewsItem from './NewsItem'
 
 export class News extends Component {
 
-  constructor() {
-    super();
-    console.log('Hello I am a constructor from News component');
+    constructor(){
+        super();
+        this.state = {
+            articles: [],
+            loading: false,
+            page:1
+        }
+    }
 
-    this.state = {
-      articles: [],
-      loading: false,
+    async componentDidMount(){
+        let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=cc6a310ae70f4a82be489063830cc9e6&page=1pageSize=20";
+        let data = await fetch(url);
+        let parsedData = await data.json()
+        console.log(parsedData);
+        this.setState({articles: parsedData.articles, totalResults: parsedData.totalResults})
+    }
 
+    handlePrevClick = async ()=>{
+        console.log("Previous");
+        let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=cc6a310ae70f4a82be489063830cc9e6&page=${this.state.page - 1}&pageSize=20`;
+        let data = await fetch(url);
+        let parsedData = await data.json()
+        console.log(parsedData);
+        this.setState({
+            page: this.state.page - 1,
+            articles: parsedData.articles
+        })
 
     }
-  }
 
-  async componentDidMount(){
-    let url = "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=cc6a310ae70f4a82be489063830cc9e6";
-    let data = await fetch(url);
-    let parsedData = await data.json()
-    console.log(parsedData);
-    this.setState({ articles: parsedData.articles })
-  }
+    handleNextClick = async ()=>{
+        console.log("Next");
+        if (this.state.page + 1 > Math.ceil(this.state.totalResults/20)){
 
-  render() {
-    console.log("render")
-    return(
-        <div className="container my-3">
-          <h1>XamNewz - Top Headlines</h1>
-          <div className="row">
-            {/*map() creates a new array from calling a function for every array element.*/}
-            {this.state.articles.map((element)=>{
-              return <div className="col-md-4" key={element.url}>
-                <NewsItem title={element.title?element.title:""} description={element.description?element.description:""} imageUrl={element.urlToImage} newsUrl={element.url}/>
-              </div>
-            })}
-          </div>
-        </div>
-    )
-  }
-}
+        }
+        else{
+            let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=cc6a310ae70f4a82be489063830cc9e6&page=${this.state.page + 1}&pageSize=20`;
+            let data = await fetch(url);
+            let parsedData = await data.json()
+            console.log(parsedData);
+            this.setState({
+                page: this.state.page + 1,
+                articles: parsedData.articles
+            })
+        }
+    }
+
+    render() {
+        return (
+            <div className="container my-3">
+                <h1>NewsMonkey - Top Headlines</h1>
+                <div className="row">
+                    {this.state.articles.map((element)=>{
+                        return <div className="col-md-4" key={element.url}>
+                            <NewsItem title={element.title?element.title:""} description={element.description?element.description:""} imageUrl={element.urlToImage} newsUrl={element.url}/>
+                        </div>
+                    })}
+                </div>
+                <div className="container d-flex justify-content-between">
+                    <button disabled={this.state.page<=1} type="button" className="btn btn-dark" onClick={this.handlePrevClick}> &larr; Previous</button>
+                    <button type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
+                </div>
+            </div>
+        )
+    }}
 
 
 
